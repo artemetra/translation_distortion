ee1a089d5afbfda9cd78b11dc8a3f1f7538c8a93
artemetra <artemlukin15@gmail.com>
2021-08-05 19:22:23 +0300

    refactoring

--- googletrans_dist.py
+++ googletrans_dist.py
@@ -2,18 +2,19 @@ from googletrans import Translator
 import random
 from languages import lang_codes
 
-input_text = str(input())
-output = ""
-iterations = 10
-
 translator = Translator()
-input_lang = translator.detect(input_text).lang
-output = input_text 
+def distort(input_text: str, iterations=10) -> str:
+    output = input_text 
+    input_lang = translator.detect(input_text).lang
 
-for i in range(iterations):
-    name, code = random.choice(list(lang_codes.items()))
-    print(str(name))
-    output = translator.translate(output, dest=code).text
-    # print(str(output))
-output = translator.translate(output, dest=input_lang).text
-print(output)
+    for i in range(iterations):
+        code, name = random.choice(list(lang_codes.items()))
+        print(f"{i}: {name}")
+        output = translator.translate(output, dest=code).text
+    output = translator.translate(output, dest=input_lang).text
+    return output
+
+if __name__ == '__main__':
+    input_text = str(input("Enter text to be distorted: "))
+    distorted = distort(input_text, 10)
+    print(distorted)
